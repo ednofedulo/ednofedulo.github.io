794f21b533581c0d2ee94f2c46d8d965f8ec1f3e
Edno Fedulo <edno.moraes@gmail.com>
2017-01-14 20:08:23 -0300

    novo site

---
+++ mb1/js/site.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function(){ 
+    jQuery("#gallery-projetos").unitegallery({
+        gallery_theme:"tiles"
+        ,tiles_type:"columns"
+    }); 
+    jQuery("#gallery-produtos").unitegallery({
+        gallery_theme:"tiles",
+        tiles_type:"columns"
+    }); 
+}); 
+        
